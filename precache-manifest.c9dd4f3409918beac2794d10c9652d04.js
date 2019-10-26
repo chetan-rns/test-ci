7e7137f158e75431c50ccb1443a830dce80d7328
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-26 13:27:47 +0000

    Deploy chetan-rns/test-ci to github.com/chetan-rns/test-ci.git:gh-pages

---
+++ precache-manifest.c9dd4f3409918beac2794d10c9652d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7449a8d7b48ac6eb94971e92fcf3cc",
+    "url": "/test-ci/index.html"
+  },
+  {
+    "revision": "8013af43426611c52db5",
+    "url": "/test-ci/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "b922911e1dec3e2d52aa",
+    "url": "/test-ci/static/js/2.38db2f4a.chunk.js"
+  },
+  {
+    "revision": "8013af43426611c52db5",
+    "url": "/test-ci/static/js/main.2fb0e5a0.chunk.js"
+  },
+  {
+    "revision": "3324f20d5ea1a4a79d32",
+    "url": "/test-ci/static/js/runtime-main.4e7969d4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test-ci/static/media/logo.25bf045c.svg"
+  }
+]);
